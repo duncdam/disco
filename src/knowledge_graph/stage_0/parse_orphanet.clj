4534ababcb6e586472945689bb61d0e5835428be
Duncan Dam <duncan.dam@reifyhealth.com>
2022-10-01 17:08:07 -0500

    Modify parsing codes for doid and orphanet

--- src/knowledge_graph/stage_0/parse_orphanet.clj
+++ src/knowledge_graph/stage_0/parse_orphanet.clj
@@ -2,20 +2,21 @@
   (:require
    [clj-http.client :as client]
    [clojure.data.xml :as d-xml]
-   [clojure.data.zip.xml :refer [text xml-> xml1->]]
-   [clojure.string :as str]
+   [clojure.data.zip.xml :refer [text attr xml->]]
    [clojure.zip :as z]
+   [clojure.string :as str]
    [knowledge-graph.module.module :as kg]))
 
 (defn orphanet-map
   [data]
   (let [z (z/xml-zip data)]
-    (for [id (xml-> z :Disorder :OrphaCode text)
-          name (xml-> z :Disorder :Name text)      
-          s (zipmap (xml-> z :Disorder :ExternalReferenceList :ExternalReference :Source text)
-                    (xml-> z :Disorder :ExternalReferenceList :ExternalReference :Reference text))
-          synonym (xml-> z :Disorder :SynonymList :Synonym text)]
-      {:id id :name name :synonym synonym :ref_source (first s) :ref_source_id (second s)})))
+    (for [id (xml-> z :Class (attr :rdf/about))
+          name (xml-> z :Class :label text)      
+          subClassOf (xml-> z :Class :subClassOf (attr :rdf/resource))
+          dbXref (xml-> z :Class :hasDbXref text)
+          synonym (xml-> z :Class :alternative_term text)
+          source_id (xml-> z :Class :notation text)]
+      {:id id :name name :subClassOf subClassOf :dbXref dbXref :synonym synonym :source_id source_id})))
 
 (defn get-orphanet
   [url output-path]
@@ -24,16 +25,14 @@
    :body
    d-xml/parse
    :content
-   (filter #(= (:tag %) :DisorderList))
-   first
-   :content
-   (filter #(= (:tag %) :Disorder))
+   (filter #(= (:tag %) :Class))
    (map orphanet-map)
    (apply concat)
+   (filter #(not= (str/lower-case (:source_id %)) "clinical subtype"))
    distinct
-   (kg/write-csv [:id :name :synonym :ref_source :ref_source_id] output-path)))
+   (kg/write-csv [:id :name :subClassOf :dbXref :synonym :source_id] output-path)))
 
 (defn run[]
-  (let [url "http://www.orphadata.com/data/xml/en_product1.xml"
+  (let [url "https://www.orphadata.com/data/ontologies/ordo/last_version/ORDO_en_4.1.owl"
         output-path-orphanet "./resources/stage_0_outputs/orphanet.csv"]
     (get-orphanet url output-path-orphanet)))
