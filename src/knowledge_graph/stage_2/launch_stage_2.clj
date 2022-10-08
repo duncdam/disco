54e0ed4b7665b9a73632689a04f91f3c34899dc8
Duncan Dam <duncan.dam@reifyhealth.com>
2022-10-08 14:38:54 -0500

    Adding stage 2 for relationships

--- src/knowledge_graph/stage_2/launch_stage_2.clj
+++ src/knowledge_graph/stage_2/launch_stage_2.clj
@@ -1,15 +1,12 @@
 (ns knowledge-graph.stage-2.launch-stage-2
   (:require
    [knowledge-graph.stage-2.stage-hasDbXref-rel :as hasDbXref]
-   [knowledge-graph.stage-2.stage-altLabel-rel :as altLabel]
    [knowledge-graph.stage-2.stage-prefLabel-rel :as prefLabel]
    [clojure.tools.logging :as log]))
 
 (defn -main []
   (hasDbXref/run)
   (log/info "finished staging hasDbXref relationships")
-  (altLabel/run)
-  (log/info "finished staging altLabel relationship")
   (prefLabel/run)
   (log/info "finished staging prefLabel relationship"))
   
