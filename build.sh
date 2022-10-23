f3d296b4117a37a4665b7e2f297b1a3e77aa31e7
Duncan Dam <duncan.dam@reifyhealth.com>
2022-10-22 20:52:47 -0500

    Update indexes for build

--- build.sh
+++ build.sh
@@ -29,7 +29,20 @@ docker exec $CONTAINER_ID bin/neo4j-admin import --database=dbdiseasemapping  --
 echo "RESTARTING CONTAINER WITH NEW GRAPH"
 docker-compose restart
 
-sleep 30
+sleep 60
 
 echo "SET INDEX"
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MODEL(model)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :DOID(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :EFO(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :HPO(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :ICD9CM(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :ICD10CM(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MEDDRA(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MEDGEN(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MESH(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MONDO(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :NCIT(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :ORPHANET(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :SNOMEDCT(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :UMLS(source_id)'
+docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :SYNONYM(name)'
