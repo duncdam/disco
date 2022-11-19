3f2f457a5b416b0e5245ad8ccf3843b581b25806
Duncan Dam <duncan.dam@reifyhealth.com>
2022-11-19 09:50:18 -0600

    Stage-1 to created nodes

--- src/knowledge_graph/stage_1/stage_disease_nodes.clj
+++ src/knowledge_graph/stage_1/stage_disease_nodes.clj
@@ -13,9 +13,6 @@
            (map #(assoc % :source source))
            (map #(assoc % :name (:label %)))
            (map #(assoc % :label source))
-           (map #(assoc % :source_id (cond
-              (contains? % :source_id) (:source_id %)
-              :else (:id %))))
            (map #(assoc % :id (cond
               (str/includes? (:id %) "_") (:id %)
               :else (str/join "_" [(:source %) (:id %)]))))
@@ -29,18 +26,19 @@
         hpo (load-disease "stage_0_outputs/hpo.csv" "HPO")
         mondo (load-disease "stage_0_outputs/mondo.csv" "MONDO")
         icd9 (load-disease "stage_0_outputs/icd9.csv" "ICD9CM")
+        icdo (load-disease "stage_0_outputs/icdo.csv" "ICDO-3")      
         icd10 (load-disease "stage_0_outputs/icd10.csv" "ICD10CM")
         icd11 (load-disease "stage_0_outputs/icd11.csv" "ICD11")
         meddra (load-disease "stage_0_outputs/meddra.csv" "MEDDRA")
         medgen (load-disease "stage_0_outputs/medgen.csv" "MEDGEN")
-        mesh-des (load-disease "stage_0_outputs/mesh_descriptor.csv" "MESH")
+        mesh-des (load-disease "stage_0_outputs/mesh_des.csv" "MESH")
         mesh-scr (load-disease "stage_0_outputs/mesh_scr.csv" "MESH")
         ncit (load-disease "stage_0_outputs/ncit.csv" "NCIT")
         orphanet (load-disease "stage_0_outputs/orphanet.csv" "ORPHANET")
         snomedct (load-disease "stage_0_outputs/snomedct.csv" "SNOMEDCT")
         umls (load-disease "stage_0_outputs/umls.csv" "UMLS")
         kegg (load-disease "stage_0_outputs/kegg.csv" "KEGG")        
-        disease-nodes (concat doid efo hpo mondo orphanet
+        disease-nodes (concat doid efo hpo mondo orphanet icdo
                               icd9 icd10 snomedct umls meddra
                               medgen mesh-des mesh-scr ncit kegg icd11)]
         (->> (distinct disease-nodes)
