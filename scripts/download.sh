65008a04e768302f1a421b4dcf002ca814f3f3a3
Duncan Dam <duncan.dam@reifyhealth.com>
2022-11-22 21:27:46 -0600

    update scripts

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,22 +1,19 @@
 #!/bin/zsh
+set -euo pipefail
 
-cleanShell()
-{
+cleanShell() {
   #clean shell
-  if [[ ! -z "$ZSH_VERSION" ]];
-    then
-      exec zsh
-  elif [[ ! -z "$BASH_VERSION" ]];
-    then 
-      exec bash --login
+  if [[ ! -z "$ZSH_VERSION" ]]; then
+    exec zsh
+  elif [[ ! -z "$BASH_VERSION" ]]; then
+    exec bash --login
   fi
 }
 
-while getopts "s:" opt
-do
-   case "$opt" in
-      s ) SOURCE=$OPTARG ;;
-   esac
+while getopts "s:" opt; do
+  case "$opt" in
+  s) SOURCE=$OPTARG ;;
+  esac
 done
 
 if [[ -z $SOURCE ]]; then
@@ -34,9 +31,21 @@ elif [[ $SOURCE == "icd9" ]]; then
   DOWNLOAD_URLS=($SNOMED_ICD9_URL $ICD9_URL)
 elif [[ $SOURCE == "icd11" ]]; then
   DOWNLOAD_URLS=($ICD11_URL $ICD11_MAPPING_URL)
+elif [[ $SOURCE == "icd10" ]]; then
+  DOWNLOAD_URLS=($ICD10_URL)
+elif [[ $SOURCE == 'icdo' ]]; then
+  DOWNLOAD_URLS=($ICDO_URL)
+elif [[ $SOURCE == "ncit" ]]; then
+  DOWNLOAD_URLS=($NCIT_URL)
+elif [[ $SOURCE == "mondo" ]]; then
+  DOWNLOAD_URLS=($MONDO_URL)
+elif [[ $SOURCE == "mesh" ]]; then
+  DOWNLOAD_URLS=($MESH_DES_URL $MESH_SCR_URL)
+elif [[ $SOURCE == "orphanet" ]]; then
+  DOWNLOAD_URLS=($ORPHANET_URL $ORPHANET_SUBCLASS_URL)
 elif [[ $SOURCE == "all" ]]; then
-  DOWNLOAD_URLS=($SNOMED_ICD10_URL $SNOMED_CORE_URL $SNOMED_ORPHANET_URL $SNOMED_MedDRA_URL $SNOMED_ORPHANET_URL $SNOMED_MAPSET_URL $SNOMED_ICD9_URL $ICD9_URL $UMLS_URL $ICD11_URL $ICD11_MAPPING_URL)
-else 
+  DOWNLOAD_URLS=($SNOMED_ICD10_URL $SNOMED_CORE_URL $SNOMED_ORPHANET_URL $SNOMED_MedDRA_URL $SNOMED_ORPHANET_URL $SNOMED_MAPSET_URL $SNOMED_ICD9_URL $ICD9_URL $UMLS_URL $ICD11_URL $ICD11_MAPPING_URL $ICD10_URL $NCIT_URL $ICDO_URL $MONDO_URL $ORPHANET_URL $ORPHANET_SUBCLASS_URL $MESH_DES_URL $MESH_SCR_URL)
+else
   echo "Wrong source"
 fi
 
@@ -47,13 +56,13 @@ if [[ ! -d $OUTPUT_PATH ]]; then
   mkdir $OUTPUT_PATH
 fi
 
-for URL in $DOWNLOAD_URLS[@]; do 
+for URL in $DOWNLOAD_URLS[@]; do
   FILE=$(echo $URL | rev | cut -d"/" -f 1 | rev)
   echo "Downloading $FILE"
 
-  if [[ $URL == *".nlm"* ]]; then
+  if [[ $URL == *"download.nlm.nih.gov"* ]]; then
     curl "$NLM_END_POINT?url=$URL&apiKey=$API_KEY" -o $OUTPUT_PATH/$FILE
-  else 
+  else
     curl $URL -o $OUTPUT_PATH/$FILE
   fi
 
@@ -61,5 +70,7 @@ for URL in $DOWNLOAD_URLS[@]; do
     unzip -o -d $OUTPUT_PATH $OUTPUT_PATH/$FILE
     rm $OUTPUT_PATH/$FILE
   fi
-  
+
 done
+
+cleanShell
--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,61 +1,58 @@
 #!/bin/zsh
+set -euo pipefail
 
-if [[ ! -d ./resources/stage_0_outputs ]];
-  then 
-    mkdir ./resources/stage_0_outputs
+if [[ ! -d ./resources/stage_0_outputs ]]; then
+  mkdir ./resources/stage_0_outputs
 fi
 
-if [[ ! -d ./resources/stage_1_outputs ]];
-  then 
-    mkdir ./resources/stage_1_outputs
+if [[ ! -d ./resources/stage_1_outputs ]]; then
+  mkdir ./resources/stage_1_outputs
 fi
 
-if [[ ! -d ./resources/stage_2_outputs ]];
-  then 
-    mkdir ./resources/stage_2_outputs
+if [[ ! -d ./resources/stage_2_outputs ]]; then
+  mkdir ./resources/stage_2_outputs
 fi
 
+echo "###################################"
+echo "STARTING STAGE 0"
+echo "###################################"
+
+clj -M:stage-0
 
 echo "###################################"
-echo "STARTING STAGE 0"                 
-echo "###################################"
-
-clj -M:stage-0                  
-
-echo "###################################"
-echo "STAGE 0 FINISHED"                 
+echo "STAGE 0 FINISHED"
 echo "###################################"
 
 echo "\n"
 
 echo "###################################"
-echo "STARTING STAGE 1"                 
+echo "STARTING STAGE 1"
 echo "###################################"
 
 clj -M:stage-1
 
 echo "###################################"
-echo "STAGE 1 FINISHED"                 
+echo "STAGE 1 FINISHED"
 echo "###################################"
 
 echo "\n"
 
 echo "###################################"
-echo "STARTING STAGE 2"                 
+echo "STARTING STAGE 2"
 echo "###################################"
 
 clj -M:stage-2
 
 echo "###################################"
-echo "STAGE 2 FINISHED"                 
+echo "STAGE 2 FINISHED"
 echo "###################################"
 
 echo "###################################"
-echo "STARTING STAGE 3"                 
+echo "STARTING STAGE 3"
 echo "###################################"
 
 clj -X:stage-3
 
 echo "###################################"
-echo "STAGE 3 FINISHED"                 
+echo "STAGE 3 FINISHED"
 echo "###################################"
