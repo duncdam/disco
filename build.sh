5e62c74532a121d292f7dd0cb59071d06a236cd0
Duncan Dam <duncan.dam@reifyhealth.com>
2022-11-24 22:12:37 -0600

    Update build script

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/zsh
 set -euo pipefail
 
+while getopts "s:" opt; do
+  case "$opt" in
+  s) SOURCE=$OPTARG ;;
+  esac
+done
+
 echo "DOWNLOAD ... "
 
-source ./scripts/download.sh
+source ./scripts/download.sh -s $SOURCE
 
 echo "FINISH DOWNLOADING!"
 
@@ -13,38 +19,8 @@ source ./scripts/run_pipeline.sh
 
 echo "FINISH RUNNING PIPELINE!"
 
-echo "BUILDING IMAGE"
-docker-compose build
+echo "CREATE NEO4J GRAPH DATABASE FOR DISEASE MAPPING"
 
-echo "SPINNING UP CONTAINER"
-docker-compose up -d
+source ./scripts/build_service.sh
 
-echo "GET DOCKER ID"
-CONTAINER_ID="$(docker ps -aqf 'name=^disease-mapping')"
-echo $CONTAINER_ID
-
-echo "LOAD GRAPH"
-docker exec $CONTAINER_ID bin/neo4j-admin import --database=dbdiseasemapping --delimiter TAB --high-io=true --skip-duplicate-nodes=true --multiline-fields=true --force \
-  --nodes='import/nodes.csv' \
-  --relationships='import/relationships.csv'
-
-echo "RESTARTING CONTAINER WITH NEW GRAPH"
-docker-compose restart
-
-sleep 60
-
-echo "SET INDEX"
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :DOID(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :EFO(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :HPO(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :ICD9CM(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :ICD10CM(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MEDDRA(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MEDGEN(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MESH(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :MONDO(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :NCIT(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :ORPHANET(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :SNOMEDCT(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :UMLS(source_id)'
-docker exec $CONTAINER_ID bin/cypher-shell -u $NEO4J_USER -p $NEO4J_PASS 'create index on :SYNONYM(name)'
+exec zsh
