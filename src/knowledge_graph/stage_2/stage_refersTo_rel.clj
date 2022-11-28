baf8354aca382942f4aa8e2660793fc109608696
Duncan Dam <duncan.dam@reifyhealth.com>
2022-11-27 21:36:27 -0600

    Update refersTo relationship

--- src/knowledge_graph/stage_2/stage_refersTo_rel.clj
+++ src/knowledge_graph/stage_2/stage_refersTo_rel.clj
@@ -11,43 +11,53 @@
   (with-open [file (io/reader (io/resource file-path))]
     (->> (csv/read-csv file :separator \tab)
          kg/csv->map
-         (mapv #(select-keys % [:start_id :end_id])))))
+         (mapv #(select-keys % [:start_id :type :end_id])))))
+
+(defn create-reference-pair
+  [hasDbXref ref-key group-key]
+  (let [hasDbXref_1 (->> (map #(set/rename-keys % {group-key :start_id ref-key :ref}) hasDbXref)
+                         (map #(select-keys % [:start_id :ref])))
+        hasDbXref_2 (->> (map #(set/rename-keys % {group-key :end_id ref-key :ref}) hasDbXref)
+                         (map #(select-keys % [:end_id :ref])))
+        joined_hasDbXref (kg/joiner hasDbXref_1 hasDbXref_2 :ref :ref kg/inner-join)]
+    (->> (filter #(not= (:start_id %) (:end_id %)) joined_hasDbXref)
+         (group-by (juxt :start_id :end_id)))))
 
 (def file-path "stage_2_outputs/hasDbXref_rel.csv")
 (def output-path "./resources/stage_2_outputs/refersTo_rel.csv")
 (def hasDbXref (load-dbXref file-path))
-(def groupBy (let [hasDbXref_1 (->> (map #(set/rename-keys % {:start_id :start_id :end_id :ref}) hasDbXref)
-                                    (map #(select-keys % [:start_id :ref])))
-                   hasDbXref_2 (->> (map #(set/rename-keys % {:start_id :end_id :end_id :ref}) hasDbXref)
-                                    (map #(select-keys % [:end_id :ref])))
-                   joined_hasDbXref (kg/joiner hasDbXref_1 hasDbXref_2 :ref :ref kg/inner-join)]
-               (->> (filter #(not= (:start_id %) (:end_id %)) joined_hasDbXref)
-                    (group-by (juxt :start_id :end_id)))))
+(def groupBy (concat (create-reference-pair hasDbXref :end_id :start_id)
+                     (create-reference-pair hasDbXref :start_id :end_id)))
 (def data-map  (->> (map #(into {} {:start_id (first (first %)) :end_id (second (first %)) :hasDbXref (second %)}) groupBy)
-                    (map #(assoc % :hasDbXref (map (fn [x] into () (:end_id x)) (:hasDbXref %))))))
+                    (map #(assoc % :hasDbXref (map (fn [x] into () (:end_id x)) (:hasDbXref %))))
+                    (filter #(not (str/blank? (:start_id %))))
+                    (filter #(not (str/blank? (:end_id %))))))
 
 (defn run
   [_]
-  (let [hasDbXref (->> (map #(set/rename-keys % {:start_id :hasDbXref_start :end_id :hasDbXref_end}) hasDbXref)
-                       (map #(select-keys % [:hasDbXref_start :hasDbXref_end])))
-        refersTo-fw (->> (map #(assoc % :start_type (first (str/split (:start_id %) #"_"))) data-map)
+  (let [refersTo-fw (->> (map #(assoc % :start_type (first (str/split (:start_id %) #"_"))) data-map)
                          (map #(assoc % :end_type (first (str/split (:end_id %) #"_"))))
                          ;; make sure two terms are from different ontology for refersTo relationship
                          (filter #(not= (:start_type %) (:end_type %)))
-                         ;; refersTo between terms have at least 2 common cross reference
-                         ;; if between ICD terms, need to be at least 4 common cross reference
                          (map #(assoc % :number_common_xRef (count (:hasDbXref %))))
+                         ;; refersTo between terms have at least 2 common cross reference
+                         ;; if between ICD terms, need to be at least 5 common cross reference
                          (filter #(cond
                                     (and (str/includes? (:start_type %) "ICD") (str/includes? (:end_type %) "ICD")) (>= (:number_common_xRef %) 4)
+                                    (and (str/includes? (:start_type %) "ICD") (str/includes? (:end_type %) "PHECODE")) (>= (:number_common_xRef %) 4)
+                                    (and (str/includes? (:start_type %) "PHECODE") (str/includes? (:end_type %) "ICD")) (>= (:number_common_xRef %) 4)
                                     :else (>= (:number_common_xRef %) 2)))
                          (map #(assoc % :type "refersTo"))
                          (map #(select-keys % [:start_id :type :end_id :number_common_xRef])))
-        ;; remove relationships already linked by hasDbXref
-        refersTo-rel-fw (->> (kg/joiner refersTo-fw hasDbXref :start_id :hasDbXref_start kg/inner-join)
-                             (filter #(not= (:end_id %) (:hasDbXref_end %)))
-                             (map #(select-keys % [:start_id :type :end_id :number_common_xRef])))
-        refersTo-rel-bw (->> (map #(set/rename-keys % {:start_id :end_id :end_id :start_id}) refersTo-rel-fw)
-                             (map #(select-keys % [:start_id :type :end_id :number_commnon_xRef])))
-        refersTo-rel (->> (concat refersTo-rel-fw refersTo-rel-bw)
-                          distinct)]
-    (kg/write-csv [:start_id :type :end_id :number_common_xRef] output-path refersTo-rel)))
+        refersTo-rel  (->> (concat (map #(assoc % :number_common_xRef 0) hasDbXref) refersTo-fw)
+                           (group-by (juxt :start_id :end_id))
+                           (map #(into {} {:start_id (first (first %)) :end_id (second (first %)) :type (second %)}))
+                           (map #(assoc % :type (map (fn [x] into () (:type x)) (:type %))))
+                           (map #(assoc % :type (distinct (:type %))))
+                           (map #(assoc % :count (count (:type %))))
+                           ;; remove relationships already linked by hasDbXref
+                           (filter #(= (:count %) 1))
+                           (filter #(= (first (:type %)) "refersTo"))
+                           (map #(assoc % :type (first (:type %))))
+                           (map #(select-keys % [:start_id :type :end_id])))]
+    (kg/write-csv [:start_id :type :end_id  :hasDbXref_start :hasDbXref_end :number_common_xRef] output-path refersTo-rel)))
